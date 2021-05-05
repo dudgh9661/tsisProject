3417bc6ab77504ad87f63892a634951a8d78e5cd
pdc <dongcheoldc@gmail.com>
2021-05-05 17:13:30 +0900

    required 페이지 DTO 수정

--- src/main/java/kr/co/tsis/education/required/DTOS/requiredLectureDTO.java
+++ src/main/java/kr/co/tsis/education/required/DTOS/requiredLectureDTO.java
@@ -1,4 +1,40 @@
 package kr.co.tsis.education.required.DTOS;
 
 public class requiredLectureDTO {
+    private int requiredLectureId;
+    private String lectureTitle;
+    private String academyName;
+    private int lectureId;
+
+    public int getRequiredLectureId() {
+        return requiredLectureId;
+    }
+
+    public void setRequiredLectureId(int requiredLectureId) {
+        this.requiredLectureId = requiredLectureId;
+    }
+
+    public String getLectureTitle() {
+        return lectureTitle;
+    }
+
+    public void setLectureTitle(String lectureTitle) {
+        this.lectureTitle = lectureTitle;
+    }
+
+    public String getAcademyName() {
+        return academyName;
+    }
+
+    public void setAcademyName(String academyName) {
+        this.academyName = academyName;
+    }
+
+    public int getLectureId() {
+        return lectureId;
+    }
+
+    public void setLectureId(int lectureId) {
+        this.lectureId = lectureId;
+    }
 }
