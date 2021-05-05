999871e7a639379278b6138b84789a69ecfdd2e9
pdc <dongcheoldc@gmail.com>
2021-05-05 17:08:44 +0900

    admin controller 수정

--- src/main/java/kr/co/tsis/education/admin/adminController.java
+++ src/main/java/kr/co/tsis/education/admin/adminController.java
@@ -1,12 +1,8 @@
 package kr.co.tsis.education.admin;
 import kr.co.tsis.education.admin.DTOS.lectureDTO;
-import kr.co.tsis.education.controller.TestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,31 +14,35 @@ import java.util.List;
 public class adminController {
     @Autowired
     private adminService service;
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(adminController.class);
 
     @GetMapping("/main")
     public String main(){
         LOGGER.debug("void");
         return "admin/main";
     }
+    @ResponseBody
     @GetMapping("/getAdmin")
     public List<lectureDTO> getAdmin(){
         LOGGER.debug("void");
         return service.getAdmin();
     }
+    @ResponseBody
     @GetMapping("/getUser") //getUser/?searchvalue=1
     public List<lectureDTO> getUser(HttpServletRequest request){
         LOGGER.debug("request",request);
         String s = request.getParameter("searchvalue");
         return service.getUser(s);
     }
+    @ResponseBody
     @PostMapping("/setAdmin")
-    public int setAdmin(@RequestParam("emp_id") String empId){
+    public int setAdmin(@RequestParam("empId") String empId){
         LOGGER.debug("empId",empId);
         return service.setAdmin(empId);
     }
+    @ResponseBody
     @PostMapping("/delAdmin")
-    public int delAdmin(@RequestParam("emp_id") String empId){
+    public int delAdmin(@RequestParam("empId") String empId){
         LOGGER.debug("empId",empId);
         return service.delAdmin(empId);
     }
