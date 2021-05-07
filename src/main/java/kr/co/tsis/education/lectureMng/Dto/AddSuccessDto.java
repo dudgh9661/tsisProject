6ca04fb365054793ac17838c98712edfa4797258
Kimyeongho <dudgh9661@naver.com>
2021-05-07 18:27:39 +0900

    update : update Dto for add function() in Controller return value

---
+++ src/main/java/kr/co/tsis/education/lectureMng/Dto/AddSuccessDto.java
@@ -0,0 +1,10 @@
+package kr.co.tsis.education.lectureMng.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AddSuccessDto {
+    private final boolean isAdded;
+}
