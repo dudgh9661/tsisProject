3a73e2a2a18e86da15543c123e97289fe03cb35f
hellochaeyoung <cyahn0831@gmail.com>
2021-05-09 17:56:25 +0900

    [add] manager/guide.{css, mustache, js}

---
+++ src/main/resources/static/js/guide-ajax.js
@@ -0,0 +1,42 @@
+
+function loadGuide(){
+
+	$.ajax({
+		url: '/guide/getGuide',
+		method: 'GET',
+		success: function(data) {
+		    console.log(data);
+			$('#guide-textarea').empty();
+            var html;
+            html = '<textarea id="noticeText" rows="10" guide-id = "' + data.guideId  + '">' + data.contents + '</textarea>';
+			$('#guide-textarea').append(html);
+		},
+		error: function(x, s, e) {
+			console.log(x, s, e);
+		}
+	});
+}
+
+loadGuide();
+
+function saveGuide() {
+
+    const guideId = document.querySelector("#noticeText").getAttribute("guide-id");
+    const text = document.querySelector("#noticeText").value;
+
+    var data = {"guideID" : guideId, "contents" : text};
+
+    $.ajax({
+    		url: '/guide/setGuide',
+    		method: 'POST',
+    		contentType : 'application/json',
+    		data : JSON.stringify(data),
+    		success: function(data) {
+                console.log(data);
+    		},
+    		error: function(x, s, e) {
+    			console.log(x, s, e);
+    		}
+    	});
+
+}
