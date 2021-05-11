b620af9244a9bf1bded8ec0e197aad15ed8ea811
진슬기 <gongbu761@gmail.com>
2021-05-11 01:34:26 +0000

    Update CategoryPageController.java

--- src/main/java/kr/co/tsis/education/categoryPage/CategoryPageController.java
+++ src/main/java/kr/co/tsis/education/categoryPage/CategoryPageController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @Controller
 @RequestMapping(value = "/CategoryPage")
